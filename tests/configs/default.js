0591fd4785ba672c8bb398a1b798947b74967cb1
Folkvir <folkvir@outlook.fr>
2018-04-24 19:55:02 +0200

    update config to be conformed

--- tests/configs/default.js
+++ tests/configs/default.js
@@ -1,10 +1,10 @@
 module.exports = {
-  clients: 10,
+  clients: 196,
   timeout: 10 * 60 * 1000,
   round: 2,
   datasets: [
     { name: 'diseasome', data: "../data/diseasome/fragments/", queries: "../data/diseasome/queries/queries.json", results: "../data/diseasome/results/", withoutQueries: ['q91.json', 'q92.json', 'q61.json', 'q53.json']},
-    // { name: 'linkedmdb', data: "../data/linkedmdb/fragments/", queries: "../data/linkedmdb/queries/queries.json", results: "../data/linkedmdb/results/", withoutQueries: []},
+    { name: 'linkedmdb', data: "../data/linkedmdb/fragments/", queries: "../data/linkedmdb/queries/queries.json", results: "../data/linkedmdb/results/", withoutQueries: []},
     // { name: 'geocoordinates', data: "../data/geocoordinates/fragments/", queries: "../data/geocoordinates/queries/queries.json", results: "../data/geocoordinates/results/", withoutQueries: []}
   ],
   options: {
--- tests/configs/full-son.js
+++ tests/configs/full-son.js
@@ -1,5 +1,5 @@
 module.exports = {
-  clients: 296,
+  clients: 196,
   round: 100,
   timeout: Infinity,
   datasets: [
