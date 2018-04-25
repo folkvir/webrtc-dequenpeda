4e5d68ea1078fbda8451e6b6dea0a1874878e50f
Folkvir <folkvir@outlook.fr>
2018-04-25 15:49:09 +0200

    improve xp script to load all xp async

--- tests/test-pm2.js
+++ tests/test-pm2.js
@@ -1,11 +1,37 @@
 const shell = require('shelljs')
 
+// need to use pm2 start tests/xp-pm2.js --no-autorestart
 const commands = [
   "node --max_old_space_size=4092 ./tests/query-normal-test-wo-webrtc --config ./tests/configs/default.js",
   //"node --max_old_space_size=100000 ./tests/query-normal-test-wo-webrtc --config ./tests/configs/full.js",
   //"node --max_old_space_size=100000 ./tests/query-normal-test-wo-webrtc --config ./tests/configs/full-son.js",
 ]
+let finished = 0
 
 commands.forEach(command => {
-  shell.exec(command)
+  exec(command).then(() => {
+    finished++
+    done()
+  })
 })
+
+const done = () => {
+  if(commands.length === finished.length) process.exit(0)
+}
+
+function exec(command) {
+  return new Promise((resolve, reject) => {
+    const child = shell.exec(command)
+    child.stdout.on('data', (data) => {
+      console.log(data)
+    });
+    child.stderr.on('data', (data) => {
+      console.log(data)
+    });
+    child.on('close', (code) => {
+      console.log(`Command: ${command} exited with code ${code}`);
+      resolve()
+    });
+  })
+
+}
