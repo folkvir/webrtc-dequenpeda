3adf472981a1c97d6761fdea4494fc9c23cb2ff1
Folkvir <folkvir@outlook.fr>
2018-04-26 10:03:27 +0200

    modify the query timeout to be equal to (round+1)*delta

--- tests/configs/full.js
+++ tests/configs/full.js
@@ -1,7 +1,6 @@
-module.exports = {
+const config = {
   clients: 196,
   round: 800,
-  timeout: 5 * 60 * 60 * 1000,
   datasets: [
     { name: 'diseasome', data: "../data/diseasome/fragments/", queries: "../data/diseasome/queries/queries.json", results: "../data/diseasome/results/", withoutQueries: ['q91.json', 'q92.json', 'q61.json', 'q53.json']},
     { name: 'linkedmdb', data: "../data/linkedmdb/fragments/", queries: "../data/linkedmdb/queries/queries.json", results: "../data/linkedmdb/results/", withoutQueries: []},
@@ -20,3 +19,6 @@ module.exports = {
     }
   }
 }
+
+config.timeout = (config.round + 1) * config.options.foglet.rps.delta
+module.exports = config
--- tests/xp-pm2.js
+++ tests/xp-pm2.js
@@ -1,21 +1,20 @@
 const shell = require('shelljs')
 
 // need to use pm2 start tests/xp-pm2.js --no-autorestart
+const round = 3
 const commands = [
   // "node --max_old_space_size=4092 ./tests/query-normal-test-wo-webrtc --config ./tests/configs/default.js",
   "node --max_old_space_size=100000 ./tests/query-normal-test-wo-webrtc --config ./tests/configs/full.js",
   "node --max_old_space_size=100000 ./tests/query-normal-test-wo-webrtc --config ./tests/configs/full-son.js",
 ]
-
-commands.forEach(command => {
-  exec(command).then(() => {
-    finished++
-    done()
+let finished = 0
+for(let i = 0; i < round; i++) {
+  commands.forEach(command => {
+    exec(command).then(() => {
+      finished++
+      if((commands.length*round) === finished) process.exit(0)
+    })
   })
-})
-
-const done = () => {
-  if(commands.length === finished.length) process.exit(0)
 }
 
 function exec(command) {
