9c1098fa86052ef1bab6894d2f2763fda2a0f794
Folkvir <folkvir@outlook.fr>
2018-04-26 17:43:22 +0200

    update deps

--- tests/graphs/graph.js
+++ tests/graphs/graph.js
@@ -45,15 +45,23 @@ function nameFromId(id) {
   return n+'-I'+n+'-O'
 }
 
+
+
 function datad3(data) {
   // data2graph
   const dataParsed = {
     links: [],
     nodes: []
   }
+  let group = new Map()
+  let revertedGroup = new Map()
   data.forEach(node => {
+    if(!revertedGroup.has(node.name)) {
+      revertedGroup.set(node.name, group.size+1)
+      group.set(group.size+1, node.name)
+    }
     dataParsed.nodes.push({
-      group: node.name,
+      group: revertedGroup.get(node.name),
       id: name(node),
       name: node.inview.slice(0, -2),
     })
@@ -78,55 +86,55 @@ function graph(data, id) {
 
 
   let svg = d3.select("#d3").append("svg:svg").style("width", 800).style("height", 600)
-  width = +svg.attr("width")
-  height = +svg.attr("height")
+  width = 800
+  height = 600
   console.log(width, height)
 
-  let color = d3.scaleOrdinal(d3.schemeCategory20);
+  let color = d3.scaleOrdinal(d3.schemePaired);
+  console.log(color, d3.schemePaired)
 
-  let simulation = d3.forceSimulation()
-      .force("link", d3.forceLink().id(function(d) { return d.id; }))
+  let simulation = d3.forceSimulation(data.nodes)
+      .force("link", d3.forceLink(data.links).id(function (d) {return d.id;}).distance(100).strength(1))
       .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2));
+      .force("center", d3.forceCenter(width/2, height/2));
 
 
 
-  let link = svg.append("g")
+  let links = svg.append("g")
       .attr("class", "links")
-    .selectAll("line")
-    .data(data.links)
-    .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+      .selectAll("line")
+      .data(data.links)
+      .enter().append("line")
+      .attr("stroke-width", function(d) { return d.value })
+      .attr("fill", (d) => color(d.group))
+      .attr("stroke", "black")
 
-  let node = svg.append("g")
+  let nodes = svg.append("g")
       .attr("class", "nodes")
-    .selectAll("circle")
-    .data(data.nodes)
-    .enter().append("circle")
+      .selectAll("circle")
+      .data(data.nodes)
+      .enter().append("circle")
       .attr("r", 5)
       .attr("fill", function(d) { return color(d.group); })
       .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged));
+      .on("start", dragstarted)
+      .on("drag", dragged));
 
-  node.append("title")
-      .text(function(d) { return d.id; });
+  nodes.append("title")
+      .text(function(d) { return d.name; });
 
   simulation
       .nodes(data.nodes)
       .on("tick", ticked);
 
-  simulation.force("link")
-      .links(data.links);
-
   function ticked() {
-    link
+    links
         .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 
-    node
+    nodes
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
   }
@@ -142,9 +150,9 @@ function graph(data, id) {
     d.fy = d3.event.y;
   }
 
-  // function dragended(d) {
-  //   if (!d3.event.active) simulation.alphaTarget(0);
-  //   d.fx = null;
-  //   d.fy = null;
-  // }
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
 }
