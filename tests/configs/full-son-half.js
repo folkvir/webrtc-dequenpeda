c64da67d3242831a28f48e76ffe66669ae056a4e
Folkvir <folkvir@outlook.fr>
2018-05-15 13:06:03 +0200

    back to xp config

--- tests/configs/full-son-half.js
+++ tests/configs/full-son-half.js
@@ -5,7 +5,7 @@ const config = {
   round: 100,
   datasets: [
     { name: 'diseasome', data: "../data/diseasome/fragments/", queries: "../data/diseasome/queries/queries.json", results: "../data/diseasome/results/", withoutQueries: ['q91.json', 'q92.json', 'q61.json', 'q53.json']},
-    // { name: 'linkedmdb', data: "../data/linkedmdb/fragments/", queries: "../data/linkedmdb/queries/queries.json", results: "../data/linkedmdb/results/", withoutQueries: []}
+    { name: 'linkedmdb', data: "../data/linkedmdb/fragments/", queries: "../data/linkedmdb/queries/queries.json", results: "../data/linkedmdb/results/", withoutQueries: []}
   ],
   options: {
     storeWorker: true, // activate the worker for the store or not
